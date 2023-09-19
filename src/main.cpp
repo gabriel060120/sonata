415ba75a0d845ee08cfcab29d8c9035c0027db55
Gabriel Patrocinio <gabriel060120@gmail.com>
2023-09-18 20:02:16 -0400

    feat: add fps indicator

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,9 @@ using namespace sf;
 int main() {
     auto window = std::make_shared<sf::RenderWindow>(sf::VideoMode(1280, 720),"Plataforma 2D", sf:: Style::Titlebar | sf::Style::Close);
     window->setPosition(Vector2i(80,0));
-    // window->setFramerateLimit(60);
-
+    // window->setFramerateLimit(120);
+    int fpsCounter = 0;
+    float timerUpdateFps;
     const float heightFloor = 100.f;
     float timeClock = 0.f;
     float actionInterval = 1.f;
@@ -29,12 +30,22 @@ int main() {
     // backgroundTexture.loadFromFile("./assets/img/bg.jpg");
     // Sprite backgroundSprite(backgroundTexture);
 
+    //sound effects
     sf::SoundBuffer metronome;
     sf::Sound soundMetronome;
     if(!metronome.loadFromFile("./../audio/sound_effects/metronome.wav")){
         std::cout << std::endl << "Nao foi possivel carregar metronomo";
     }
     soundMetronome.setBuffer(metronome);
+
+    //text
+    sf::Font font;
+    sf::Text fpsIndicator;
+    font.loadFromFile("../fonts/PixelBloated.ttf");
+    fpsIndicator.setFont(font);
+    fpsIndicator.setString(std::to_string(fpsCounter) + "FPS");
+    fpsIndicator.setPosition(Vector2f(window->getSize().x - 150, 0));
+    fpsIndicator.setScale(Vector2f(0.5f, 0.5f));
 
     // Texture floorTexture;
     RectangleShape floor(Vector2f(window->getSize().x, heightFloor));
@@ -48,11 +59,17 @@ int main() {
         timeClock = gameClock.getElapsedTime().asSeconds();
         gameClock.restart();
         timerToAction += timeClock;
-        
+        timerUpdateFps += timeClock;
+        fpsCounter = (1/timeClock) + 1;
+
+        if(timerUpdateFps >= 0.25) {
+            timerUpdateFps = 0;
+            fpsIndicator.setString(std::to_string(fpsCounter) + "FPS");
+        }
         if(timerToAction >= actionInterval) {
             allowedAction = true;
             timerToAction = 0.f;
-            soundMetronome.play();
+            // soundMetronome.play();
             // cout << "\x1B[0m>> Gatilho <<" << endl;
         } else {
             allowedAction = false;
@@ -65,13 +82,18 @@ int main() {
                 window->close();
             }
         }
-        
+        //updates
         player.update(timeClock, allowedAction);
         if(enemy.getLife() > 0)
             enemy.update(timeClock, allowedAction);
+        
+        
         window->clear();
+
+        //draw
         // window->draw(backgroundSprite);
         window->draw(floor);
+        window->draw(fpsIndicator);
         if(enemy.getLife() > 0)
             enemy.render();
         player.render();
